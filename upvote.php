641e4722f359b468b59ef765992aa626d4877248
ab9649 <105522725+ab9649@users.noreply.github.com>
2022-05-14 16:18:37 -0400

    update user status on upvotes

--- upvote.php
+++ upvote.php
@@ -13,14 +13,40 @@ $AnsID = $_GET['AnsID'];
 $type = $_GET['type'];
 $QID = $_GET['QID'];
 
+#get the userid of the answer
+$sql = "SELECT UserID from answer where AnsID = $AnsID";
+$res = $con->query($sql);
+$aobj = $res->fetch_assoc();
+$auserID = $aobj["UserID"];
+
+#insert/deletion when pressing upvote button
 $insertUpvoteSQL = "INSERT INTO Thumbed_up VALUES ($UserID, $AnsID)";
 $deleteUpvoteSQL = "DELETE FROM Thumbed_up WHERE AnsID = $AnsID AND UserID = $UserID";
 
+#insertion
 if ($type == 'upvote') {
     $upvoteRes = $con->query($insertUpvoteSQL);
+
 } else if ($type == 'cancel') {
     $upvoteRes = $con->query($deleteUpvoteSQL);
 }
+#check total votes of answer user after insert/deltion
+$checknumvotes = "SELECT count(*) as votes from thumbed_up as T, answer as A where T.AnsID = A.AnsID and A.UserID = $auserID";
+$check = $con->query($checknumvotes);
+$vobj = $check->fetch_assoc();
+$votes = $vobj["votes"];
+
+#update status based on total votes
+if ($votes > 2){
+    $updatestatus = "UPDATE USERS set statusID = 3 where userID = $auserID";
+}
+else if ($votes > 1){
+    $updatestatus = "UPDATE USERS set statusID = 2 where userID = $auserID";
+}
+else{
+    $updatestatus = "UPDATE USERS set statusID = 1 where userID = $auserID";
+}
+$update = $con->query($updatestatus);
 
 header('Location: question.php?QID='.$QID);
 ?>
